5d0abd9c3ffcda55d25d5c83c1eca41ed5e56995
Lakshya5876 <lakshyahappy@gmail.com>
2025-10-15 11:38:42 +0530

    Add DriverDashboard

---
+++ frontend/src/pages/DriverDashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DriverDashboard() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Driver Dashboard</h2>
+      <div>Here drivers can view and accept rides. (Feature to be implemented)</div>
+    </div>
+  );
+}
